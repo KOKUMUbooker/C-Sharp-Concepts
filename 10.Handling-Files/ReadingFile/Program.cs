63e64b8648e3d0be98e6b298144b97fd13d4c739
KOKUMUbooker <bookerochieng20@gmail.com>
2025-11-06 11:30:24 +0300

    Complete reading files

---
+++ 10.Handling-Files/ReadingFile/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ReadingFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            // Approach 1 :
+            string text = System.IO.File.ReadAllText(@"sample.txt");
+            Console.WriteLine($"Sample.txt file contains (Method 1 - using ReadAllText()) : \n{text}");
+
+            Console.WriteLine();
+            // Approach 2 :
+            string[] lines = System.IO.File.ReadAllLines(@"sample.txt");
+            Console.WriteLine("Sample.txt file contains (Approach 2 - using ReadAllLines()) : ");
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
