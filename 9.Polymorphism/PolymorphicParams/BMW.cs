1780b8332486e20bde5d64ae07092a2ca605f92e
KOKUMUbooker <bookerochieng@gmail.com>
2025-11-06 08:14:46 +0300

    Complete use of the sealed keyword section

--- 9.Polymorphism/PolymorphicParams/BMW.cs
+++ 9.Polymorphism/PolymorphicParams/BMW.cs
@@ -16,7 +16,14 @@ namespace PolymorphicParams
             Console.WriteLine($"{this.Brand} {this.Model} has HP of {this.HP} & is {this.Color} in color.");
         }
 
-        public override void Repair()
+        // Added sealed keyword to prevent overriding by 
+        // a child class of BMW
+        // The sealed keyword must come before the override
+        // keyword
+        // sealed keyword only works on methods that can be
+        // overridden ie those marked by base class using 
+        // virtual keyword
+        public sealed override void Repair()
         {
             Console.WriteLine($"{this.Brand} {this.Model} has been repaired!");
         }
