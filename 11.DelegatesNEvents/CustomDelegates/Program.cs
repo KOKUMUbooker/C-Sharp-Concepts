a93ca5401833baa036b177567a940e288d850b72
KOKUMUbooker <bookerochieng20@gmail.com>
2025-11-07 17:19:00 +0300

    Complete Delegates basics

---
+++ 11.DelegatesNEvents/CustomDelegates/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomDelegates
+{
+    class Program
+    {
+        // defined filter delegate type for filtering Person instances
+        public delegate bool FilterPeopleDelegate(Person p);
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>()
+            {
+                new Person("John",40),
+                new Person("Elliot",15),
+                new Person("Pennywise",1000),
+                new Person("Antony",18)
+            };
+
+            Console.Clear();
+            DisplayPeople("Minors", people, IsMinor);
+            DisplayPeople("Adults", people, IsAdult);
+            DisplayPeople("Seniors", people, IsSenior);
+        }
+
+        static void DisplayPeople(string title, List<Person> people, FilterPeopleDelegate filter)
+        {
+            Console.WriteLine($"{title} : ");
+            foreach (Person p in people)
+            {
+                // Check if this person passes the filter
+                if (filter(p))
+                {
+                    Console.WriteLine($"{p.Name}, {p.Age} years old");
+                }
+
+            }
+            Console.WriteLine();
+        }
+
+        // ================== FILTERS ===================
+        static bool IsMinor(Person p)
+        {
+            return p.Age < 18;
+        }
+
+        static bool IsAdult(Person p)
+        {
+            return p.Age >= 18;
+        }
+        
+        static bool IsSenior(Person p)
+        {
+            return p.Age >= 65;
+        }
+    }
+}
---
+++ 11.DelegatesNEvents/DelegatesBasics/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DelegatesBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string>() {
+                "John",
+                "Jane",
+                "Alice",
+                "Pandemonium"
+            };
+
+            Console.Clear(); 
+            Console.Write("Before : ");
+            foreach (string name in names)
+            {
+                Console.Write($"{name}, ");
+            }
+            Console.WriteLine();
+            
+            names.RemoveAll(RemoveFilteredNames);
+
+            Console.Write("After : ");
+            foreach (string name in names)
+            {
+                Console.Write($"{name}, ");
+            }
+            Console.WriteLine();
+        }
+        static bool RemoveFilteredNames(string s){
+            return s.Contains("P");
+        }
+    }
+}
